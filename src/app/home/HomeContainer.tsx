967e5349ac7f12d81e2be662b02c50a895675a03
Hruday kumar reddy <hrudaykumar11@gmail.com>
2024-03-30 21:31:01 +0530

    fix: updated ui changes

--- src/app/home/HomeContainer.tsx
+++ src/app/home/HomeContainer.tsx
@@ -1,22 +1,35 @@
 'use client';
-import React from 'react';
+import React, { useState } from 'react';
 import Value from './Value';
-import Timer from './Timer';
 import Features from './Features';
 import Team from './Team';
 import StayUpdated from './StayUpdated';
 import Footer from './Footer';
 import Register from './RegisterContainer';
+import SuccessFooter from './SuccessFooter';
 
 const HomeContainer = () => {
+    const [isRegistered, setIsRegistered] = useState<boolean>(false);
+    const [isSuccess, setIsSuccess] = useState<boolean>(false);
+
+    const handleRegister = () => {
+        setIsRegistered(!isRegistered);
+    };
+
+    const handleSuccess = () => {
+        setIsSuccess(!isSuccess);
+    }
+
+
     return (
         <div className="flex flex-col min-h-screen">
-            <Register />
+            <Register isRegistered={isRegistered} handleRegister={handleRegister} isSuccess={isSuccess} handleSuccess={handleSuccess} />
+            <SuccessFooter isSuccess={isSuccess} />
             <Value />
-            <Timer />
             <Features />
             <Team />
             <StayUpdated />
+
             <Footer />
         </div>
     );
