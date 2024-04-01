87df53a591bfb7079f16271a4eb0f10cf92f6dd2
Angel Lakra <angel.lakra@chainlabs.in>
2024-04-01 22:47:11 +0530

    added footer

--- src/app/home/Footer.tsx
+++ src/app/home/Footer.tsx
@@ -1,16 +1,22 @@
-const Footer = () => {
+import Image from "next/image";
+import Link from "next/link";
 
-    return (
-        <div className="flex flex-col">
-            <div className='h-auto w-full'>
-                <img
-                    src="footer.svg"
-                    alt="footer"
-                    className="h-full w-full object-cover"
-                />
-            </div>
+const Footer = () => {
+  return (
+    <div className="flex justify-between items-center px-32 py-8">
+      <Link href="/">
+        <div className="flex items-center cursor-pointer">
+          <Image src="icon.svg" alt="icon" width={64} height={64} />
+          <p className=" from-white to-gray-500 pl-2 font-sans font-extrabold text-4xl bg-gradient-to-b text-transparent bg-clip-text">
+            ANTIGRAVITY
+          </p>
         </div>
-    );
+      </Link>
+      <p className=" from-white to-gray-500 pl-2 font-sans font-extrabold text-xl bg-gradient-to-b text-transparent bg-clip-text uppercase">
+        2024 &copy; Copyrights reserved
+      </p>
+    </div>
+  );
 };
 
 export default Footer;
