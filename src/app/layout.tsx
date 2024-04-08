069cfe7d2cf1eb65d67898c05f107f0185f21686
Angel Lakra <angel.lakra@chainlabs.in>
2024-04-08 22:21:36 +0530

    edited proxy config

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import "./globals.css";
 import "@rainbow-me/rainbowkit/styles.css";
 import RainbowKitContext from "@/components/RainbowKit";
 import { Toaster } from "react-hot-toast";
+import IMAGEKIT from "./home/images";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -22,8 +23,17 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Toaster />
-        <RainbowKitContext>{children}</RainbowKitContext>
+        <div className="hidden md:block">
+          <Toaster />
+          <RainbowKitContext>{children}</RainbowKitContext>
+        </div>
+        <div className="block md:hidden">
+          <div
+            className={`max-h-screen h-screen w-screen bg-[url('${IMAGEKIT.HERO_LANDING}')]`}
+          >
+            <h1>Join Us On Desktop! </h1>
+          </div>
+        </div>
       </body>
     </html>
   );
