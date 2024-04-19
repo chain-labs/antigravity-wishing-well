2072b75388a8673de3cf992e8b53b292abab755f
AayushCodes <aayushcodes@gmail.com>
2024-04-20 03:28:52 +0530

    added mobile responsiveness to value section

--- src/app/home/Value.tsx
+++ src/app/home/Value.tsx
@@ -5,72 +5,74 @@ import IMAGEKIT from "./images";
 const Value = () => {
   return (
     <div className="flex flex-col bg-agblack relative items-center">
-      <div className="absolute top-0 left-0 w-screen h-[1000px] mix-blend-luminosity">
+      <div className="absolute top-0 left-0 w-screen h-full mix-blend-luminosity">
         <div className="relative h-full w-full -z-1">
-          <Image
-            src={IMAGEKIT.VALUE_BG}
-            alt="value section background"
-            className=""
-            fill
-          />
+          <Image src={IMAGEKIT.VALUE_BG} alt="value section background" fill />
         </div>
       </div>
-      <div className="px-10 py-24 z-10 w-3/4 max-w-[1280px]">
-        <p className="font-sans text-[64px] font-black text-agwhite">
+      <div className="px-10 py-24 z-10 w-full sm:w-3/4 sm:max-w-[1280px]">
+        <p className="font-sans text-5xl sm:text-[64px] font-black text-agwhite">
           The Value Of The Project
         </p>
-        <div className="flex mt-12 flex-col gap-6">
-          <div className="flex gap-6">
-            <ValueItem
-              imgSrc="https://ik.imagekit.io/xlvg9oc4k/Antigravity/limited_supply.svg"
-              imgText="limited_supply"
-              itemTitle="limited supply / no inflation"
-            />
-            <ValueItem
-              imgSrc="https://ik.imagekit.io/xlvg9oc4k/Antigravity/limited_minting.svg"
-              imgText="limited_minting"
-              itemTitle="limited minting phases"
-            />
+        <div className="mt-12">
+          <div className="grid sm:grid-cols-2 w-full sm:w-fit gap-6">
+            <div>
+              <ValueItem
+                imgSrc="https://ik.imagekit.io/xlvg9oc4k/Antigravity/limited_supply.svg"
+                imgText="limited_supply"
+                itemTitle="limited supply / no inflation"
+              />
+            </div>
+            <div>
+              <ValueItem
+                imgSrc="https://ik.imagekit.io/xlvg9oc4k/Antigravity/limited_minting.svg"
+                imgText="limited_minting"
+                itemTitle="limited minting phases"
+              />
+            </div>
+            <div>
+              <ValueItem
+                imgSrc="https://ik.imagekit.io/xlvg9oc4k/Antigravity/yields_rewards.svg"
+                imgText="yields_rewards"
+                itemTitle="yield that rewards holders"
+              />
+            </div>
+            <div>
+              <ValueItem
+                imgSrc="https://ik.imagekit.io/xlvg9oc4k/Antigravity/giveaway_tokens.svg"
+                imgText="giveaway_tokens"
+                itemTitle="giveaways in liquid tokens"
+              />
+            </div>
           </div>
-          <div className="flex gap-6">
-            <ValueItem
-              imgSrc="https://ik.imagekit.io/xlvg9oc4k/Antigravity/yields_rewards.svg"
-              imgText="yields_rewards"
-              itemTitle="yield that rewards holders"
-            />
-            <ValueItem
-              imgSrc="https://ik.imagekit.io/xlvg9oc4k/Antigravity/giveaway_tokens.svg"
-              imgText="giveaway_tokens"
-              itemTitle="giveaways in liquid tokens"
-            />
-          </div>
-          <div className="flex gap-6">
-            <ValueItem
-              imgSrc="https://ik.imagekit.io/xlvg9oc4k/Antigravity/transfarrable_nft.svg"
-              imgText="transfarrable_nft"
-              itemTitle="Transferable nfts That have actual cash value"
-            />
-            <ValueItem
-              imgSrc="https://ik.imagekit.io/xlvg9oc4k/Antigravity/token_crisis.svg"
-              imgText="token_crisis"
-              itemTitle="Programmed Scarcity like you’ve never seen before"
-            />
-            <ValueItem
-              imgSrc="https://ik.imagekit.io/xlvg9oc4k/Antigravity/heart_nft.svg"
-              imgText="heart_nft"
-              itemTitle="nfts with a story and heart behind them"
-            />
+          <div className="grid mt-6 gap-6 sm:grid-cols-3 w-full sm:w-fit ">
+            <div>
+              <ValueItem
+                imgSrc="https://ik.imagekit.io/xlvg9oc4k/Antigravity/transfarrable_nft.svg"
+                imgText="transfarrable_nft"
+                itemTitle="Transferable nfts That have actual cash value"
+              />
+            </div>
+            <div>
+              <ValueItem
+                imgSrc="https://ik.imagekit.io/xlvg9oc4k/Antigravity/token_crisis.svg"
+                imgText="token_crisis"
+                itemTitle="Programmed Scarcity like you’ve never seen before"
+              />
+            </div>
+            <div>
+              <ValueItem
+                imgSrc="https://ik.imagekit.io/xlvg9oc4k/Antigravity/heart_nft.svg"
+                imgText="heart_nft"
+                itemTitle="nfts with a story and heart behind them"
+              />
+            </div>
           </div>
         </div>
       </div>
-      <Image
-        src={IMAGEKIT.ROCK_BIG}
-        alt="big rock"
-        width={700}
-        height={700}
-        quality={20}
-        className="absolute top-1/2 -translate-y-1/2 right-0"
-      />
+      <div className="h-[800px] w-[800px] sm:h-[700px] sm:w-[700px] absolute bottom-0 sm:top-1/2 sm:-translate-y-1/2 sm:right-0">
+        <Image src={IMAGEKIT.ROCK_BIG} alt="big rock" fill quality={20} />
+      </div>
     </div>
   );
 };
