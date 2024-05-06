e28bf50ae7f5a324f4b37c49175ede994930daac
AayushCodes <aayushcodes@gmail.com>
2024-05-07 00:56:35 +0530

    timer fixes

--- src/app/home/images.ts
+++ src/app/home/images.ts
@@ -8,17 +8,14 @@ const IMAGEKIT = {
     "https://ik.imagekit.io/chainlabs/AntiGravity/value_bg_0OIOfjXmi.png",
   STARS_BG:
     "https://ik.imagekit.io/chainlabs/AntiGravity/feature-bg_YvomhJEJPd.svg",
-  ASTRO:
-    "https://ik.imagekit.io/chainlabs/AntiGravity/astro_ZOv2puRdFm.svg",
+  ASTRO: "https://ik.imagekit.io/chainlabs/AntiGravity/astro_ZOv2puRdFm.svg",
   TEAM_BG:
     "https://ik.imagekit.io/chainlabs/AntiGravity/bapspatil_A_floating_dark_triangular_rock_inside_an_open_space__e02ced69-ab73-46a0-88c5-d9c8916514af_upscayl_4x_remacri%201_XbMtyH0Dn.png",
-  ROCK1:
-    "https://ik.imagekit.io/chainlabs/AntiGravity/rock1_1IlCgXMXX.svg",
-  ROCK2:
-    "https://ik.imagekit.io/chainlabs/AntiGravity/rock2_HWsl6qHLUo.svg",
+  ROCK1: "https://ik.imagekit.io/chainlabs/AntiGravity/rock1_1IlCgXMXX.svg",
+  ROCK2: "https://ik.imagekit.io/chainlabs/AntiGravity/rock2_HWsl6qHLUo.svg",
   ROCK_BIG:
     "https://ik.imagekit.io/chainlabs/AntiGravity/rock-big_uV2L6ZRBCj.svg",
-  SHIP: "https://ik.imagekit.io/chainlabs/AntiGravity/timer-ship_A_TtqHziq0.svg",
+  SHIP: "https://ik.imagekit.io/chainlabs/AntiGravity/ship_tvus8g8sp.png?updatedAt=1715022820385",
 };
 
 export default IMAGEKIT;
