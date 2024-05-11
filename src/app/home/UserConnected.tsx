772467c41fae90c0644703d87045ea5095fb13d2
AayushCodes <aayushcodes@gmail.com>
2024-05-11 08:50:30 +0530

    added chain and account modal interactions to navbar

--- src/app/home/UserConnected.tsx
+++ src/app/home/UserConnected.tsx
@@ -8,20 +8,20 @@ export const UserConnected: React.FC = () => {
   return (
     <div className="flex text-lg">
       <ConnectButton.Custom>
-        {({ chain, openChainModal, mounted }) => {
+        {({ chain, openChainModal, mounted, openAccountModal }) => {
           if (chain && chain.unsupported) {
             return (
               <div>
                 <div
+                  className="flex text-red-400 w-full h-full bg-agblack gap-2 items-center rounded-lg cursor-pointer focus:outline-none peer"
                   onClick={openChainModal}
-                  className="flex text-red-400 w-full h-full bg-agblack gap-2 items-center rounded-lg peer cursor-pointer focus:outline-none peer"
                 >
                   <PiWarningCircle className="text-brred w-8 h-8" />
-                  <p className="uppercase bg-gradient-to-b font-extrabold text-brred text-transparent bg-clip-text text-lg">
+                  <p className="uppercase font-extrabold text-brred text-transparent bg-clip-text z-[100]">
                     {condenseAddress(`${account.address}`)}
                   </p>
                 </div>
-                <div className="peer-hover:flex hidden absolute bg-brred -bottom-8 rounded font-normal text-base px-8">
+                <div className="peer-hover:flex hidden absolute bg-brred -bottom-8 z-10 rounded font-normal text-base px-8">
                   Wrong network
                 </div>
               </div>
@@ -31,18 +31,19 @@ export const UserConnected: React.FC = () => {
             //   location.reload();
             // setCurrentChain(chain.name as string);
             return (
-              <div
-                className="flex w-full h-full bg-agblack gap-2 items-center rounded-lg cursor-pointer focus:outline-none"
-                onClick={openChainModal}
-              >
+              <div className="flex w-full h-full bg-agblack gap-2 items-center rounded-lg cursor-pointer focus:outline-none">
                 {chain.hasIcon ? (
                   <>
                     <img
                       src={chain.iconUrl}
                       alt={chain.name}
                       className="w-8 h-8 rounded-full"
+                      onClick={openChainModal}
                     />
-                    <p className="uppercase bg-gradient-to-b font-extrabold from-[#B4EBF8] to-[#789DFA] text-transparent bg-clip-text">
+                    <p
+                      className="uppercase bg-gradient-to-b font-extrabold from-[#B4EBF8] to-[#789DFA] text-transparent bg-clip-text"
+                      onClick={openAccountModal}
+                    >
                       {condenseAddress(`${account.address}`)}
                     </p>
                   </>
