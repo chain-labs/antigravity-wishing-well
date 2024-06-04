50f014bf600e69fb7bced0790373c6bfa52eb38f
Pratham <chudasamapratham142@gmail.com>
2024-06-04 16:06:08 +0530

    stoping serverside rendering of some components

--- src/app/model/page.tsx
+++ src/app/model/page.tsx
@@ -1,11 +1,14 @@
 "use client";
 
 import React from "react";
-import Header from "../home/Header";
 import dynamic from "next/dynamic";
 import ReactLenis from "@studio-freight/react-lenis";
 
 const CanvasRendering = dynamic(() => import("./CanvasRendering"), {
+	ssr: false,
+});
+
+const Header = dynamic(() => import("../home/Header"), {
 	ssr: false,
 });
 
