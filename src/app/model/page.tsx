f508c38cd0faf7a2893d8e95a05a47a984aab1aa
Pratham <chudasamapratham142@gmail.com>
2024-06-04 20:28:27 +0530

    z index fixed

--- src/app/model/page.tsx
+++ src/app/model/page.tsx
@@ -25,56 +25,60 @@ export default function Model() {
 					<div className="fixed top-0 w-full z-50 items-center pt-12 px-4">
 						<Header />
 					</div>
-					<div className="w-full h-[100vh] 10 fixed top-0 left-0">
-						<CanvasRendering />
-					</div>
+					
 					{/* <Spinner /> */}
-					<div className="flex justify-center items-center gap-1 w-full h-[150vh] pt-[60vh]">
-						<div className="animate-pulse w-full h-full bg-slate-500"></div>
-						<div className="animate-pulse w-full h-full bg-slate-500"></div>
-						<div className="animate-pulse w-full h-full bg-slate-500"></div>
-					</div>
+					<div className="z-100">
+						<div className="flex justify-center items-center gap-1 w-full h-[150vh] pt-[60vh]">
+							<div className="w-full h-full bg-slate-500"></div>
+							<div className="w-full h-full bg-slate-500"></div>
+							<div className="w-full h-full bg-slate-500"></div>
+						</div>
 
-					<div className="flex flex-col justify-center items-center gap-8 w-full max-h-[100vh] py-[20vh]">
-						<h1 className="text-5xl text-white">Leaderboard</h1>
-						<div className="grid grid-cols-3 gap-2 h-[60vh] w-[80%]">
-							<div className="animate-pulse w-full h-full bg-slate-500"></div>
-							<div className="animate-pulse w-full h-full bg-slate-500"></div>
-							<div className="animate-pulse w-full h-full bg-slate-500"></div>
-							<div className="animate-pulse w-full h-full bg-slate-500"></div>
-							<div className="animate-pulse w-full h-full bg-slate-500"></div>
-							<div className="animate-pulse w-full h-full bg-slate-500"></div>
+						<div className="flex flex-col justify-center items-center gap-8 w-full max-h-[100vh] py-[20vh]">
+							<h1 className="text-5xl text-white">Leaderboard</h1>
+							<div className="grid grid-cols-3 gap-2 h-[60vh] w-[80%]">
+								<div className="w-full h-full bg-slate-500"></div>
+								<div className="w-full h-full bg-slate-500"></div>
+								<div className="w-full h-full bg-slate-500"></div>
+								<div className="w-full h-full bg-slate-500"></div>
+								<div className="w-full h-full bg-slate-500"></div>
+								<div className="w-full h-full bg-slate-500"></div>
+							</div>
+						</div>
+
+						<div className="flex flex-col justify-center items-center gap-8 w-full max-h-[100vh] py-[20vh]">
+							<h1 className="text-5xl text-white">
+								Here&apos;s what folks have to say!
+							</h1>
+							<div className="grid grid-cols-3 gap-2 h-[60vh] w-[80%]">
+								<div className="w-full h-full bg-slate-500"></div>
+								<div className="w-full h-full bg-slate-500"></div>
+								<div className="w-full h-full bg-slate-500"></div>
+								<div className="w-full h-full bg-slate-500"></div>
+								<div className="w-full h-full bg-slate-500"></div>
+								<div className="w-full h-full bg-slate-500"></div>
+							</div>
+						</div>
+
+						<div className="flex flex-col justify-center items-center gap-8 w-full h-[100vh]"></div>
+
+						<div className="flex flex-col justify-center items-center gap-8 w-full max-h-[100vh]  my-[40vh]">
+							<h1 className="text-5xl text-white">
+								Next Section
+							</h1>
+							<div className="grid grid-cols-3 gap-2 h-[60vh] w-[80%]">
+								<div className="w-full h-full bg-slate-500"></div>
+								<div className="w-full h-full bg-slate-500"></div>
+								<div className="w-full h-full bg-slate-500"></div>
+								<div className="w-full h-full bg-slate-500"></div>
+								<div className="w-full h-full bg-slate-500"></div>
+								<div className="w-full h-full bg-slate-500"></div>
+							</div>
 						</div>
 					</div>
 
-					<div className="flex flex-col justify-center items-center gap-8 w-full max-h-[100vh] py-[20vh]">
-						<h1 className="text-5xl text-white">
-							Here&apos;s what folks have to say!
-						</h1>
-						<div className="grid grid-cols-3 gap-2 h-[60vh] w-[80%]">
-							<div className="animate-pulse w-full h-full bg-slate-500"></div>
-							<div className="animate-pulse w-full h-full bg-slate-500"></div>
-							<div className="animate-pulse w-full h-full bg-slate-500"></div>
-							<div className="animate-pulse w-full h-full bg-slate-500"></div>
-							<div className="animate-pulse w-full h-full bg-slate-500"></div>
-							<div className="animate-pulse w-full h-full bg-slate-500"></div>
-						</div>
-					</div>
-
-					<div className="flex flex-col justify-center items-center gap-8 w-full h-[100vh]"></div>
-
-					<div className="flex flex-col justify-center items-center gap-8 w-full max-h-[100vh]  my-[40vh]">
-						<h1 className="text-5xl text-white">
-							Next Section
-						</h1>
-						<div className="grid grid-cols-3 gap-2 h-[60vh] w-[80%]">
-							<div className="animate-pulse w-full h-full bg-slate-500"></div>
-							<div className="animate-pulse w-full h-full bg-slate-500"></div>
-							<div className="animate-pulse w-full h-full bg-slate-500"></div>
-							<div className="animate-pulse w-full h-full bg-slate-500"></div>
-							<div className="animate-pulse w-full h-full bg-slate-500"></div>
-							<div className="animate-pulse w-full h-full bg-slate-500"></div>
-						</div>
+					<div className="w-full h-[100vh] 10 fixed top-0 left-0 -z-[1]">
+						<CanvasRendering />
 					</div>
 				</div>
 			</div>
