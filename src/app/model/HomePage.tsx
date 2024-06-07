98cc4793209a318a1eab4b8cce10f164d560c885
Pratham <chudasamapratham142@gmail.com>
2024-06-08 02:04:59 +0530

    responsive leaderboard

--- src/app/model/HomePage.tsx
+++ src/app/model/HomePage.tsx
@@ -29,25 +29,13 @@ export default function HomePage() {
 			<div className="flex flex-col min-h-screen min-w-screen overflow-hidden">
 				<div className="relative z-0 flex flex-col min-h-screen">
 					<div className="fixed top-0 w-full z-50 items-center pt-12 px-4">
-						{/* <Header /> */}
+						<Header />
 					</div>
 
-					<Leaderboard />
-
-					{/* <div className="z-100">
+					<div className="z-100">
 						<Hero />
 
-						<div className="flex flex-col justify-center items-center gap-8 w-full max-h-[100vh] py-[20vh]">
-							<h1 className="text-5xl text-white">Leaderboard</h1>
-							<div className="grid grid-cols-3 gap-2 h-[60vh] w-[80%]">
-								<div className="w-full h-full bg-slate-500"></div>
-								<div className="w-full h-full bg-slate-500"></div>
-								<div className="w-full h-full bg-slate-500"></div>
-								<div className="w-full h-full bg-slate-500"></div>
-								<div className="w-full h-full bg-slate-500"></div>
-								<div className="w-full h-full bg-slate-500"></div>
-							</div>
-						</div>
+						<Leaderboard />
 
 						<Testimonials />
 
@@ -72,9 +60,9 @@ export default function HomePage() {
 
 						<Newsletter />
 						<Footer />
-					</div> */}
+					</div>
 
-					{/* <div className="w-full h-[100vh] 10 fixed top-0 left-0 -z-[1]">
+					<div className="w-full h-[100vh] 10 fixed top-0 left-0 -z-[1]">
 						<CanvasRendering />
 						<StarFieldCanvas
 							count={100}
@@ -83,7 +71,7 @@ export default function HomePage() {
 							zRange={100}
 							speed={0.1}
 						/>
-					</div> */}
+					</div>
 				</div>
 			</div>
 		</div>
--- src/app/model/sections/Leaderboard.tsx
+++ src/app/model/sections/Leaderboard.tsx
@@ -7,15 +7,20 @@ import { twMerge } from "tailwind-merge";
 function TH({
 	icon,
 	heading,
+	className,
 }: {
 	icon: string | StaticImport;
 	heading: string;
+	className?: string;
 }) {
 	return (
 		<th
-			className="relative bg-[#0A0025] border-1 border-transparent bg-clip-padding flex flex-col lg:flex-row justify-between z-0 px-3 py-[10px] w-full
+			className={twMerge(
+				`relative bg-[#0A0025] border-1 border-transparent bg-clip-padding flex flex-col lg:flex-row justify-between z-0 px-3 py-[10px] w-full
             before:content-[''] before:absolute before:inset-0 before:z-[-10] before:bg-gradient-to-bl before:from-[#3C00DC] before:to-[#FF5001] before:rounded-[inherit] before:overflow-hidden before:m-[-1px]
-			after:content-[''] after:absolute after:inset-0 after:z-[-2] after:bg-agblack after:rounded-[inherit] after:overflow-hidden"
+			after:content-[''] after:absolute after:inset-0 after:z-[-2] after:bg-agblack after:rounded-[inherit] after:overflow-hidden`,
+				className
+			)}
 		>
 			<div className="flex items-center gap-2 uppercase tracking-widest text-xl from-white to-[#999999] font-sans font-extrabold bg-gradient-to-b text-transparent bg-clip-text">
 				<Image
@@ -31,6 +36,12 @@ function TH({
 	);
 }
 
+function truncatinator(str: string) {
+	return (
+		str.substring(0, 6) + "..." + str.substring(str.length - 4, str.length)
+	);
+}
+
 function TD({
 	children,
 	truncate = false,
@@ -42,17 +53,15 @@ function TD({
 }) {
 	if (truncate && children && typeof children === "string") {
 		return (
-			<td className="elative border-r-2 border-[#8275A5] bg-clip-padding flex flex-col lg:flex-row justify-between z-0 px-3 py-[10px] w-full truncate">
-				{children.substring(0, 6) +
-					"..." +
-					children.substring(children.length - 4, children.length)}
+			<td className="elative border-r-2 border-[#8275A5] bg-clip-padding hidden lg:flex flex-col lg:flex-row justify-between z-0 px-3 py-[10px] w-full truncate">
+				{truncatinator(children)}
 			</td>
 		);
 	}
 	return (
 		<td
 			className={twMerge(
-				"relative border-r-2 border-[#8275A5] bg-clip-padding flex justify-start items-center gap-4 flex-col lg:flex-row z-0 px-3 py-[10px]",
+				"relative border-r-2 border-[#8275A5] bg-clip-padding flex justify-center lg:justify-start lg:items-center gap-1 lg:gap-4 flex-col lg:flex-row z-0 px-3 py-[10px]",
 				special && "border-none"
 			)}
 		>
@@ -77,7 +86,7 @@ function TR({
 	return (
 		<tr
 			className={twMerge(
-				"grid grid-cols-[2fr_1fr_1fr] w-full border-l-2 border-b-2 border-[#8275A5]",
+				"grid grid-cols-[2fr_1fr] lg:grid-cols-[2fr_1fr_1fr] w-full border-l-2 border-b-2 border-[#8275A5]",
 				special &&
 					"bg-agyellow text-black font-extrabold w-[calc(100%_+_24px)] translate-x-[-12px] border-none rounded-lg",
 				empty &&
@@ -101,7 +110,7 @@ function Badge({
 	return (
 		<div
 			className={twMerge(
-				"relative flex items-center gap-2 justify-center font-sans font-extrabold text-white cursor-pointer rounded-full p-1 px-2 border-2 text-sm uppercase tracking-widest",
+				"relative flex items-center gap-2 justify-center font-sans font-extrabold text-white cursor-pointer rounded-full p-1 px-2 border-2 text-xs lg:text-sm uppercase tracking-widest w-fit",
 				special &&
 					"text-agyellow font-extrabold bg-gradient-to-b from-[#0A1133] to-[#142266] border-none"
 			)}
@@ -111,21 +120,39 @@ function Badge({
 	);
 }
 
+function Rank({ rank, wallet, special = false }: { rank: number; wallet: string, special?: boolean}) {
+	return (
+		<TD>
+			#{rank} <Badge special={special}>Specialist Technician</Badge>
+			<div className="flex gap-2 justify-start items-center lg:hidden">
+				<Image
+					src={require("@/app/model/assets/icons/wallet.svg")}
+					alt="hammer icon"
+					width={25}
+					height={25}
+					className={twMerge("object-cover", special && "filter-invert")}
+				/>
+				{truncatinator(wallet)}
+			</div>
+		</TD>
+	);
+}
+
 export default function Leaderboard() {
 	return (
 		<div
-			className="relative md:w-4/5 mx-4 md:mx-auto bg-[#0A0025] rounded-xl p-8 border-4 border-transparent bg-clip-padding flex flex-col lg:flex-row justify-between gap-10 z-0
+			className="relative md:w-4/5 mx-4 my-32 md:mx-auto bg-[#0A0025] rounded-xl p-8 border-4 border-transparent bg-clip-padding flex flex-col lg:flex-row justify-between gap-10 z-0
             before:content-[''] before:absolute before:inset-0 before:z-[-10] before:bg-gradient-to-bl before:from-[#5537A5] before:to-[#BF6841] before:rounded-[inherit] before:overflow-hidden before:m-[-2px]
 			after:content-[''] after:absolute after:inset-0 after:z-[-2] after:bg-agblack after:rounded-[inherit] after:overflow-hidden
         "
 		>
 			<div className="flex flex-col">
-				<div className="flex justify-start items-center gap-6">
+				<div className="flex flex-wrap justify-start items-center gap-6">
 					<h1 className="text-6xl from-white to-[#999999] font-sans font-extrabold bg-gradient-to-b text-transparent bg-clip-text">
 						Leaderboard
 					</h1>
 					<button
-						className={`relative flex items-center gap-x-2 justify-center font-sans uppercase font-extrabold tracking-widest text-agwhite cursor-pointer rounded-lg px-4 py-3 shadow-button shadow-[#414343] border-[#414343] border-2 hover:translate-y-1 transition-[all_150ms] hover:shadow-none active:bg-agblack`}
+						className={`relative flex items-center gap-x-2 justify-center font-sans uppercase font-extrabold tracking-widest text-agwhite cursor-pointer rounded-lg px-4 py-3 shadow-button shadow-[#414343] border-[#414343] border-2 hover:translate-y-1 transition-[all_150ms] hover:shadow-none active:bg-[rgba(255,255,255,0.25)]`}
 					>
 						<Image
 							src={require("@/app/model/assets/icons/refresh.svg")}
@@ -138,7 +165,7 @@ export default function Leaderboard() {
 					</button>
 				</div>
 
-				<div className="grid grid-cols-3 w-full max-w-[100%] pt-6">
+				<div className="grid grid-cols-1 lg:grid-cols-3 w-full max-w-[100%] pt-6">
 					<table className="col-span-2 w-full bg-gradient-to-b from-[#0A1133] to-[#142266] h-fit">
 						<thead className="w-full">
 							<TR th>
@@ -149,6 +176,7 @@ export default function Leaderboard() {
 								<TH
 									icon={require("@/app/model/assets/icons/wallet.svg")}
 									heading="Wallet"
+									className="hidden lg:flex"
 								/>
 								<TH
 									icon={require("@/app/model/assets/icons/points.svg")}
@@ -158,45 +186,35 @@ export default function Leaderboard() {
 						</thead>
 						<tbody className="text-lg font-medium font-general-sans text-white">
 							<TR>
-								<TD>
-									#1 <Badge>Specialist Technician</Badge>
-								</TD>
+								<Rank rank={1} wallet="0x1234567890abcdef1234567890abcdef12345678" />
 								<TD truncate>
 									0x1234567890abcdef1234567890abcdef12345678
 								</TD>
 								<TD>90,000</TD>
 							</TR>
 							<TR>
-								<TD>
-									#2 <Badge>Specialist Technician</Badge>
-								</TD>
+								<Rank rank={2} wallet="0x1234567890abcdef1234567890abcdef12345678" />
 								<TD truncate>
 									0x1234567890abcdef1234567890abcdef12345678
 								</TD>
 								<TD>90,000</TD>
 							</TR>
 							<TR>
-								<TD>
-									#3 <Badge>Specialist Technician</Badge>
-								</TD>
+								<Rank rank={3} wallet="0x1234567890abcdef1234567890abcdef12345678" />
 								<TD truncate>
 									0x1234567890abcdef1234567890abcdef12345678
 								</TD>
 								<TD>90,000</TD>
 							</TR>
 							<TR>
-								<TD>
-									#4 <Badge>Specialist Technician</Badge>
-								</TD>
+								<Rank rank={4} wallet="0x1234567890abcdef1234567890abcdef12345678" />
 								<TD truncate>
 									0x1234567890abcdef1234567890abcdef12345678
 								</TD>
 								<TD>90,000</TD>
 							</TR>
 							<TR>
-								<TD>
-									#5 <Badge>Specialist Technician</Badge>
-								</TD>
+								<Rank rank={5} wallet="0x1234567890abcdef1234567890abcdef12345678" />
 								<TD truncate>
 									0x1234567890abcdef1234567890abcdef12345678
 								</TD>
@@ -207,28 +225,21 @@ export default function Leaderboard() {
 								<></>
 							</TR>
 							<TR>
-								<TD>
-									#1234566 <Badge>Specialist Technician</Badge>
-								</TD>
+								<Rank rank={1234566} wallet="0x1234567890abcdef1234567890abcdef12345678" />
 								<TD truncate>
 									0x1234567890abcdef1234567890abcdef12345678
 								</TD>
 								<TD>90,000</TD>
 							</TR>
 							<TR special>
-								<TD>
-									#1234567{" "}
-									<Badge special>Specialist Technician</Badge>
-								</TD>
+								<Rank rank={1234567} wallet="0x1234567890abcdef1234567890abcdef12345678" special />
 								<TD truncate>
 									0x1234567890abcdef1234567890abcdef12345678
 								</TD>
 								<TD special>90,000</TD>
 							</TR>
 							<TR>
-								<TD>
-									#1234568 <Badge>Specialist Technician</Badge>
-								</TD>
+								<Rank rank={1234568} wallet="0x1234567890abcdef1234567890abcdef12345678" />
 								<TD truncate>
 									0x1234567890abcdef1234567890abcdef12345678
 								</TD>
@@ -237,13 +248,13 @@ export default function Leaderboard() {
 						</tbody>
 					</table>
 
-					<div className="relative flex flex-col w-full gap-4 pl-6 place-self-end">
+					<div className="relative flex flex-col w-full gap-4 lg:pl-6 place-self-end">
 						<Image
 							src={require("@/app/model/assets/collective-color-logo.svg")}
 							alt="leaderboard background"
 							width={100}
 							height={100}
-							className="object-cover absolute left-[10%] top-0 translate-y-[-100%] z-[-1] opacity-[50%]"
+							className="object-cover absolute bottom-0 right-0 lg:left-[10%] lg:top-0 lg:translate-y-[-100%] z-[100] opacity-[50%]"
 						/>
 						<div className="flex flex-col gap-4 p-4">
 							<h2 className="font-general-sans text-xl text-white font-medium">
