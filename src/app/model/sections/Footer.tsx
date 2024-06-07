e7681525773bd2f65dd5b524c7211113caa76ac1
Pratham <chudasamapratham142@gmail.com>
2024-06-07 21:30:57 +0530

    footer responsive

--- src/app/model/sections/Footer.tsx
+++ src/app/model/sections/Footer.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Footer() {
 	return (
-		<div className="flex justify-between items-center px-16 py-4 bg-agblack">
+		<div className="flex flex-col md:flex-row justify-start md:justify-between items-start md:items-center px-4 md:px-16 py-4 bg-agblack">
 			<div className="flex gap-4 justify-center items-center">
 				<Image
 					src={require("@/app/model/assets/logo.svg")}
@@ -14,7 +14,7 @@ export default function Footer() {
 					Antigravity
 				</div>
 			</div>
-			<div className="uppercase tracking-widest text-lg text-center from-white to-[#999999] font-sans font-extrabold bg-gradient-to-b text-transparent bg-clip-text">
+			<div className="uppercase tracking-widest text-lg md:text-center from-white to-[#999999] font-sans font-extrabold bg-gradient-to-b text-transparent bg-clip-text">
 				2024 ©️ Copyrights Reserved
 			</div>
 		</div>
