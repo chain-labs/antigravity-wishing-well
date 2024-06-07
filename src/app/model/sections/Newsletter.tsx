d4cd72a22ffaccbdcdd5ee44691f379d7529b61a
Pratham <chudasamapratham142@gmail.com>
2024-06-07 19:11:21 +0530

    padding on top and bottom and contrast of the title font added

--- src/app/model/sections/Newsletter.tsx
+++ src/app/model/sections/Newsletter.tsx
@@ -6,11 +6,11 @@ import { useState } from "react";
 export default function Newsletter() {
 	const [success, setSuccess] = useState(false);
 	return (
-		<div className="flex items-start justify-center md:w-4/5 mx-4 md:mx-auto gap-16">
+		<div className="flex items-start justify-center md:w-4/5 mx-4 md:mx-auto gap-16 my-64">
 			{success === false ? (
 				<>
 					<div className="flex flex-col gap-2">
-						<h1 className="text-6xl text-left from-white to-[#999999] font-sans font-black bg-gradient-to-b text-transparent bg-clip-text">
+						<h1 className="text-6xl text-left text-white font-sans font-black ">
 							Ignite Your <br /> Boosters.
 						</h1>
 						<p className="text-xl text-center from-white to-[#999999] font-sans font-medium bg-gradient-to-b text-transparent bg-clip-text">
@@ -23,14 +23,14 @@ export default function Newsletter() {
 							name="name"
 							id="name"
 							placeholder="Your Name"
-							className="text-white p-3 rounded-xl w-[30em] font-sans font-semibold text-lg"
+							className="text-agblack p-3 rounded-xl w-[30em] font-sans font-semibold text-lg"
 						/>
 						<input
 							type="email"
 							name="email"
 							id="email"
 							placeholder="your@email.com"
-							className="text-white p-3 rounded-xl w-[30em] font-sans font-semibold text-lg"
+							className="text-agblack p-3 rounded-xl w-[30em] font-sans font-semibold text-lg"
 						/>
 						<button
 							type="submit"
