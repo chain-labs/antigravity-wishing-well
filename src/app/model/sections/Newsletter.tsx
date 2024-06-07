6b9dfcef97b02efdeeaec33de2f0f926b607ca90
Pratham <chudasamapratham142@gmail.com>
2024-06-07 18:59:25 +0530

    newsletter section added

---
+++ src/app/model/sections/Newsletter.tsx
@@ -0,0 +1,62 @@
+"use client";
+
+import Image from "next/image";
+import { useState } from "react";
+
+export default function Newsletter() {
+	const [success, setSuccess] = useState(false);
+	return (
+		<div className="flex items-start justify-center md:w-4/5 mx-4 md:mx-auto gap-16">
+			{success === false ? (
+				<>
+					<div className="flex flex-col gap-2">
+						<h1 className="text-6xl text-left from-white to-[#999999] font-sans font-black bg-gradient-to-b text-transparent bg-clip-text">
+							Ignite Your <br /> Boosters.
+						</h1>
+						<p className="text-xl text-center from-white to-[#999999] font-sans font-medium bg-gradient-to-b text-transparent bg-clip-text">
+							Get all Antigravity updates in your inbox.
+						</p>
+					</div>
+					<form action="" className="flex flex-col gap-4">
+						<input
+							type="text"
+							name="name"
+							id="name"
+							placeholder="Your Name"
+							className="text-white p-3 rounded-xl w-[30em] font-sans font-semibold text-lg"
+						/>
+						<input
+							type="email"
+							name="email"
+							id="email"
+							placeholder="your@email.com"
+							className="text-white p-3 rounded-xl w-[30em] font-sans font-semibold text-lg"
+						/>
+						<button
+							type="submit"
+							className="uppercase tracking-widest rounded-md bg-brblue px-4 py-3 text-white font-sans font-extrabold flex justify-center items-center text-xl gap-2 w-fit"
+						>
+							<Image
+								src={require("@/app/model/assets/icons/send.svg")}
+								alt="send"
+								width={40}
+								height={40}
+							/>
+							Submit
+						</button>
+					</form>
+				</>
+			) : (
+				<div className="flex flex-col justify-center items-center gap-4">
+					<h1 className="text-6xl text-left from-white to-[#999999] font-sans font-black bg-gradient-to-b text-transparent bg-clip-text">
+						Success!
+					</h1>
+					<p className="text-xl text-center from-white to-[#999999] font-sans font-medium bg-gradient-to-b text-transparent bg-clip-text">
+						You&apos;ll get all Antigravity updates in your inbox.
+						<br /> Stay tune!.
+					</p>
+				</div>
+			)}
+		</div>
+	);
+}
