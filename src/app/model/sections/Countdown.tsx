761285587d3f91874045eaa486d178b334f493cd
Pratham <chudasamapratham142@gmail.com>
2024-06-08 11:31:31 +0530

    countdown responsiveness

--- src/app/model/sections/Countdown.tsx
+++ src/app/model/sections/Countdown.tsx
@@ -76,6 +76,65 @@ function Phase({
 	);
 }
 
+function MobilePhase({
+	activeState,
+	era,
+	phase,
+}: {
+	activeState: stateType;
+	era: stateType["era"];
+	phase: stateType["phase"];
+}) {
+	if (activeState.era === era && activeState.phase === phase) {
+		return (
+			<motion.div
+				whileInView={{
+					color: "black",
+				}}
+				initial={{ color: "black" }}
+				viewport={{ once: true }}
+				transition={{ duration: 0.5, delay: 1 }}
+				className={twMerge(
+					"relative text-5xl font-sans font-extrabold text-center bg-agyellow rounded-lg p-4 px-8"
+				)}
+			>
+				{phase}
+				<motion.div
+					whileInView={{
+						width: "100%",
+					}}
+					initial={{ width: "0%" }}
+					transition={{
+						duration: 1,
+						ease: "easeIn",
+					}}
+					viewport={{ once: true }}
+					className="absolute w-full h-full bottom-0 left-0 translate-y-[35%] translate-x-[2%] flex justify-center items-center "
+				>
+					<Image
+						src={require("@/app/model/assets/timer-pointer.svg")}
+						alt="timer-pointer"
+						width={20}
+						height={20}
+						className="mx-[8px]"
+					/>
+				</motion.div>
+			</motion.div>
+		);
+	}
+
+	return (
+		<motion.div
+			className={twMerge(
+				"text-5xl font-sans font-extrabold text-center",
+				"from-white to-[#999999] bg-gradient-to-b text-transparent bg-clip-text rounded-lg p-4 px-8"
+			)}
+		>
+			{phase}
+		</motion.div>
+	);
+}
+
 function calculateActivePhasesSlider(activeState: stateType) {
 	let activePhase = 0;
 	if (activeState.era === "wishwell") {
@@ -141,7 +200,7 @@ export default function Countdown() {
 
 	return (
 		<div
-			className="relative md:w-4/5 mx-4 md:mx-auto bg-[#0A0025] rounded-xl p-8 border-4 border-transparent bg-clip-padding flex flex-col lg:flex-row justify-between gap-10 z-0
+			className="relative w-[110%] translate-x-[-4.5%] lg:w-4/5 md:mx-auto bg-[#0A0025] rounded-xl p-8 border-4 border-transparent bg-clip-padding flex flex-col lg:flex-row justify-between gap-10 z-0
             before:content-[''] before:absolute before:inset-0 before:z-[-10] before:bg-gradient-to-bl before:from-[#5537A5] before:to-[#BF6841] before:rounded-[inherit] before:overflow-hidden before:m-[-4px]
 			after:content-[''] after:absolute after:inset-0 after:z-[-2] after:bg-[#0A0025] after:rounded-[inherit] after:overflow-hidden
         "
@@ -151,7 +210,7 @@ export default function Countdown() {
 				alt="countdown bg grid"
 				width={800}
 				height={800}
-				className="absolute inset-0 z-[-1] w-full h-full object-cover user-select-none pointer-events-none opacity-[33%]"
+				className="absolute inset-0 z-[-1] w-full h-full object-cover user-select-none pointer-events-none opacity-[66%]"
 			/>
 			<div className="flex justify-start items-start flex-col gap-4">
 				<div className="tracking-widest uppercase text-3xl text-center from-white to-[#999999] font-sans font-extrabold bg-gradient-to-b text-transparent bg-clip-text">
@@ -164,7 +223,7 @@ export default function Countdown() {
 								count={state.days}
 								setCount={() => {}}
 								modulo={10000}
-                                boxPixelSize={60}
+								boxPixelSize={60}
 							/>
 						</h1>
 						<p className="text-xl uppercase font-extrabold tracking-widest">
@@ -175,11 +234,11 @@ export default function Countdown() {
 					<div className="flex items-center justify-center flex-col">
 						<h1 className="text-6xl font-extrabold">
 							<DynamicNumberCounter
-                                count={state.hours}
-                                setCount={() => {}}
-                                modulo={24}
-                                boxPixelSize={60}
-                            />
+								count={state.hours}
+								setCount={() => {}}
+								modulo={24}
+								boxPixelSize={60}
+							/>
 						</h1>
 						<p className="text-xl uppercase font-extrabold tracking-widest">
 							Hours
@@ -189,11 +248,11 @@ export default function Countdown() {
 					<div className="flex items-center justify-center flex-col">
 						<h1 className="text-6xl font-extrabold">
 							<DynamicNumberCounter
-                                count={state.mins}
-                                setCount={() => {}}
-                                modulo={60}
-                                boxPixelSize={60}
-                            />
+								count={state.mins}
+								setCount={() => {}}
+								modulo={60}
+								boxPixelSize={60}
+							/>
 						</h1>
 						<p className="text-xl uppercase font-extrabold tracking-widest">
 							Mins
@@ -203,11 +262,11 @@ export default function Countdown() {
 					<div className="flex items-center justify-center flex-col">
 						<h1 className="text-6xl font-extrabold">
 							<DynamicNumberCounter
-                                count={state.secs}
-                                setCount={() => {}}
-                                modulo={60}
-                                boxPixelSize={60}
-                            />
+								count={state.secs}
+								setCount={() => {}}
+								modulo={60}
+								boxPixelSize={60}
+							/>
 						</h1>
 						<p className="text-xl uppercase font-extrabold tracking-widest">
 							Secs
@@ -216,7 +275,111 @@ export default function Countdown() {
 				</div>
 			</div>
 
-			<div className="relative flex flex-col rounded bg-gradient-to-b from-[#5730BF] to-[#15004C] p-4 z-0 overflow-hidden">
+			<div className="relative flex lg:hidden flex-col rounded bg-gradient-to-b from-[#5730BF] to-[#15004C] p-4 z-0 overflow-hidden">
+				<div className="grid grid-cols-1 gap-4">
+					<div className="relative h-full flex flex-col gap-2 p-2">
+						<motion.div
+							whileInView={{
+								height: "calc(100%)",
+							}}
+							initial={{ height: "0%" }}
+							style={{
+								width: "calc(100%)",
+							}}
+							transition={{ duration: 1 }}
+							viewport={{ once: true }}
+							className="absolute top-0 left-0 bg-gradient-to-b from-[#030404] to-[#131A1A] rounded-xl -z-10 opacity-[30%]"
+						></motion.div>
+						<div className="text-5xl text-center from-white to-[#999999] font-sans font-extrabold bg-gradient-to-b text-transparent bg-clip-text">
+							Wishwell
+						</div>
+						<div className="tracking-widest uppercase text-lg text-center from-white to-[#999999] font-sans font-extrabold bg-gradient-to-b text-transparent bg-clip-text">
+							Phase
+						</div>
+						<div className="flex justify-center items-center gap-2 relative z-0 px-auto">
+							<MobilePhase
+								activeState={state}
+								era="wishwell"
+								phase={1}
+							/>
+							<MobilePhase
+								activeState={state}
+								era="wishwell"
+								phase={2}
+							/>
+							<MobilePhase
+								activeState={state}
+								era="wishwell"
+								phase={3}
+							/>
+						</div>
+					</div>
+					<div className="flex flex-col gap-2 p-2">
+						<div className="text-5xl text-center font-sans font-extrabold text-agyellow">
+							Mining
+						</div>
+						<div className="tracking-widest uppercase text-lg text-center from-white to-[#999999] font-sans font-extrabold bg-gradient-to-b text-transparent bg-clip-text">
+							Phase
+						</div>
+						<div className="flex justify-center items-center gap-2 relative z-0 px-auto">
+							<MobilePhase
+								activeState={state}
+								era="mining"
+								phase={1}
+							/>
+							<MobilePhase
+								activeState={state}
+								era="mining"
+								phase={2}
+							/>
+							<MobilePhase
+								activeState={state}
+								era="mining"
+								phase={3}
+							/>
+						</div>
+					</div>
+					<div className="relative flex flex-col gap-2 p-2">
+						<motion.div
+							whileInView={{
+								height: "calc(100%)",
+							}}
+							initial={{ height: "0%" }}
+							style={{
+								width: "calc(100%)",
+							}}
+							transition={{ duration: 1 }}
+							viewport={{ once: true }}
+							className="absolute bottom-0 right-0 bg-gradient-to-b from-[#030404] to-[#131A1A] rounded-xl -z-10 opacity-[30%]"
+						></motion.div>
+						<div className="text-5xl text-center from-white to-[#999999] font-sans font-extrabold bg-gradient-to-b text-transparent bg-clip-text">
+							Minting
+						</div>
+						<div className="tracking-widest uppercase text-lg text-center from-white to-[#999999] font-sans font-extrabold bg-gradient-to-b text-transparent bg-clip-text">
+							Phase
+						</div>
+						<div className="flex justify-center items-center gap-2 relative z-0 px-auto">
+							<MobilePhase
+								activeState={state}
+								era="minting"
+								phase={1}
+							/>
+							<MobilePhase
+								activeState={state}
+								era="minting"
+								phase={2}
+							/>
+							<MobilePhase
+								activeState={state}
+								era="minting"
+								phase={3}
+							/>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div className="hidden relative lg:flex flex-col rounded bg-gradient-to-b from-[#5730BF] to-[#15004C] p-4 z-0 overflow-hidden">
 				<div className="grid grid-cols-3 gap-14">
 					<div className="h-full flex flex-col gap-2">
 						<div className="text-5xl text-center from-white to-[#999999] font-sans font-extrabold bg-gradient-to-b text-transparent bg-clip-text">
