b1a80056f40141546332b605da377a3facf018f4
Pratham <chudasamapratham142@gmail.com>
2024-06-16 22:29:47 +0530

    using api to get the country info using ip

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,30 +1,19 @@
-import { NextResponse, NextRequest } from "next/server";
+import { NextResponse } from "next/server";
 import { headers } from "next/headers";
 
-// This function can be marked `async` if using `await` inside
-export async function middleware(request: NextRequest) {
+export async function middleware() {
 	const forwardedFor = headers().get("x-forwarded-for");
 	const realIp = headers().get("x-real-ip");
 
-	const apiResponse = (
-		await fetch(`https://get.geojs.io/v1/ip/country/${realIp}.json`)
+	const apiResponse = await (
+		await fetch(
+			`https://get.geojs.io/v1/ip/country/${realIp ?? "0.0.0.0"}.json`
+		)
 	).json();
 
-	// if (forwardedFor) {
-	// 	return NextResponse.json({
-	// 		forwardedFor
-	// 	});
-	// }
 	return NextResponse.json({
 		realIp,
 		forwardedFor,
 		apiResponse,
 	});
-
-	// return NextResponse.redirect(new URL("/home", request.url));
 }
-
-// See "Matching Paths" below to learn more
-// export const config = {
-// 	matcher: ["/wishwell"],
-// };
