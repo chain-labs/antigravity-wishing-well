b9110450c43efa297f9f405559ff12e5f59b9265
Pratham <chudasamapratham142@gmail.com>
2024-06-20 21:36:22 +0530

    making nft present

--- src/components/Mining/MiningHero.tsx
+++ src/components/Mining/MiningHero.tsx
@@ -279,7 +279,7 @@ function NonContributed({
 }
 
 export default function MiningHero() {
-	const [state, setState] = useState<StateType>("Claiming");
+	const [state, setState] = useState<StateType>("NFT Present");
 	const [NFTHover, setNFTHover] = useState(false);
 	const NFTRef = useRef<HTMLDivElement>(null);
 	const NFTContainerRef = useRef<HTMLDivElement>(null);
