4c17e059ed8e89269a427226d8d730735d06143e
Pratham <chudasamapratham142@gmail.com>
2024-06-24 15:26:36 +0530

    dded loading state

--- src/components/Home/components/header/RegisterButton.tsx
+++ src/components/Home/components/header/RegisterButton.tsx
@@ -10,73 +10,80 @@ import { IMAGEKIT_ICONS } from "@/assets/imageKit";
 import { useEffect } from "react";
 
 interface RegisterButtonProps {
-  loading: boolean;
-  error: boolean;
-  registerIdle: boolean;
-  handleLogin: (args0: React.MouseEvent) => void;
-  setError: (value: boolean) => void;
-  handleRegister: (args0: React.MouseEvent) => void;
-  isRegistered: boolean;
+	loading: boolean;
+	error: boolean;
+	registerIdle: boolean;
+	handleLogin: (args0: React.MouseEvent) => void;
+	setError: (value: boolean) => void;
+	handleRegister: (args0: React.MouseEvent) => void;
+	isRegistered: boolean;
 }
 
 export const RegisterButton: React.FC<RegisterButtonProps> = ({
-  loading,
-  error,
-  registerIdle,
-  handleLogin,
-  setError,
-  handleRegister,
-  isRegistered,
+	loading,
+	error,
+	registerIdle,
+	handleLogin,
+	setError,
+	handleRegister,
+	isRegistered,
 }) => {
-  const account = useAccount();
-  const switchChain = useSwitchChain();
+	const account = useAccount();
+	const switchChain = useSwitchChain();
 
-  return (
-    <>
-      <Button
-        className="w-fit lg:w-fit"
-        onClick={
-          !loading
-            ? account.isConnected
-              ? checkCorrectNetwork(Number(account.chainId))
-                ? handleRegister
-                : () =>
-                    switchChain.switchChain({
-                      chainId: TEST_NETWORK ? baseSepolia.id : pulsechain.id,
-                    })
-              : handleLogin
-            : !account.isConnected
-            ? handleLogin
-            : error
-            ? () => {
-                setError(false);
-              }
-            : () => {}
-        }
-        innerText={
-          account.isConnected
-            ? checkCorrectNetwork(Number(account.chainId))
-              ? loading
-                ? !error
-                  ? "Checking your Registration"
-                  : "Recheck"
-                : !isRegistered
-                ? registerIdle
-                  ? "REGISTER NOW"
-                  : "Registering..."
-                : ""
-              : "Change Network"
-            : "CONNECT WALLET"
-        }
-        // TODO: Need to add a loading iconSrc as well when loading is passed to this button from props
-        iconSrc={IMAGEKIT_ICONS.WALLET_WHITE}
-        iconPosition="start"
-      />
-      {/* {(account.address && loading && !error) || !registerIdle ? (
+	return (
+		<>
+			<Button
+				className="w-fit lg:w-fit"
+				onClick={
+					!loading
+						? account.isConnected
+							? checkCorrectNetwork(Number(account.chainId))
+								? handleRegister
+								: () =>
+										switchChain.switchChain({
+											chainId: TEST_NETWORK
+												? baseSepolia.id
+												: pulsechain.id,
+										})
+							: handleLogin
+						: !account.isConnected
+							? handleLogin
+							: error
+								? () => {
+										setError(false);
+									}
+								: () => {}
+				}
+				innerText={
+					account.isConnected
+						? checkCorrectNetwork(Number(account.chainId))
+							? loading
+								? !error
+									? "Checking your Registration"
+									: "Recheck"
+								: !isRegistered
+									? registerIdle
+										? "REGISTER NOW"
+										: "Registering..."
+									: ""
+							: "Change Network"
+						: "CONNECT WALLET"
+				}
+				// TODO: Need to add a loading iconSrc as well when loading is passed to this button from props
+				loading={
+					account.isConnected &&
+					checkCorrectNetwork(Number(account.chainId)) &&
+					loading
+				}
+				iconSrc={IMAGEKIT_ICONS.WALLET_WHITE}
+				iconPosition="start"
+			/>
+			{/* {(account.address && loading && !error) || !registerIdle ? (
 				<div className="animate-[spin_2s_ease-out_infinite]">
 					<FiLoader />
 				</div>
 			) */}
-    </>
-  );
+		</>
+	);
 };
