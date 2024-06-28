02ec4cb97d960ebaee1dc1f6a1ed60715c63da1c
Pratham <chudasamapratham142@gmail.com>
2024-06-28 19:53:48 +0530

    added lenis smooth scroll

--- src/app/HomePage.tsx
+++ src/app/HomePage.tsx
@@ -16,6 +16,7 @@ import NFTReceipt from "@/components/Home/sections/NFTReceipt";
 import { useAccount } from "wagmi";
 import Image from "next/image";
 import { IMAGEKIT_IMAGES } from "@/assets/imageKit";
+import { ReactLenis } from "lenis/react";
 
 // const Hero = dynamic(() => import("./sections/Hero"), {
 // 	ssr: false,
@@ -46,45 +47,49 @@ export default function HomePage() {
 		};
 	}, []);
 	return (
-		<div className="bg-agblack min-h-[100vh]">
-			<div className="flex flex-col min-h-screen min-w-screen overflow-hidden">
-				<div className="relative z-0 flex flex-col min-h-screen">
-					<div className="fixed top-0 w-full z-50 items-center pt-[16px] md:pt-12 px-4">
-						<Header />
-					</div>
-					<div className="z-100">
-						<Hero />
-						<Leaderboard accountIsConnected={account.isConnected} />
-						<Testimonials />
-						<NFTReceipt />
-						<Eras />
-						<Countdown />
-						<Newsletter />
-						<Footer />
-					</div>
-					<div className="w-full h-[100vh] 10 fixed top-0 left-0 -z-[1]">
-						{smallerViewPort ? (
-							<Image
-								src={IMAGEKIT_IMAGES.MOBILE_SATURN}
-								alt="Mobile Saturn"
-								width={1920}
-								height={1080}
-								className="fixed top-0 left-0 md:hidden w-[150vw] h-fit -translate-y-1/2 mix-blend-lighten z-0 scale-[1.25] pointer-events-none select-none"
+		<ReactLenis root>
+			<div className="bg-agblack min-h-[100vh]">
+				<div className="flex flex-col min-h-screen min-w-screen overflow-hidden">
+					<div className="relative z-0 flex flex-col min-h-screen">
+						<div className="fixed top-0 w-full z-50 items-center pt-[16px] md:pt-12 px-4">
+							<Header />
+						</div>
+						<div className="z-100">
+							<Hero />
+							<Leaderboard
+								accountIsConnected={account.isConnected}
 							/>
-						) : (
-							<CanvasRendering />
-						)}
+							<Testimonials />
+							<NFTReceipt />
+							<Eras />
+							<Countdown />
+							<Newsletter />
+							<Footer />
+						</div>
+						<div className="w-full h-[100vh] 10 fixed top-0 left-0 -z-[1]">
+							{smallerViewPort ? (
+								<Image
+									src={IMAGEKIT_IMAGES.MOBILE_SATURN}
+									alt="Mobile Saturn"
+									width={1920}
+									height={1080}
+									className="fixed top-0 left-0 md:hidden w-[150vw] h-fit -translate-y-1/2 mix-blend-lighten z-0 scale-[1.25] pointer-events-none select-none"
+								/>
+							) : (
+								<CanvasRendering />
+							)}
 
-						<StarFieldCanvas
-							count={50}
-							xRange={100}
-							yRange={100}
-							zRange={100}
-							speed={0.1}
-						/>
+							<StarFieldCanvas
+								count={50}
+								xRange={100}
+								yRange={100}
+								zRange={100}
+								speed={0.1}
+							/>
+						</div>
 					</div>
 				</div>
 			</div>
-		</div>
+		</ReactLenis>
 	);
 }
