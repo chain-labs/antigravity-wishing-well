2c4a7c09e5dbb134752a291cac23e2f88f23e63b
Pratham <chudasamapratham142@gmail.com>
2024-07-01 13:45:19 +0530

    clamping text for mining

--- src/components/Mining/MiningCalculator.tsx
+++ src/components/Mining/MiningCalculator.tsx
@@ -26,50 +26,60 @@ export function InputCard({
 	dropDownSelected: number;
 	setDropDownSelected: Dispatch<SetStateAction<number>>;
 }) {
+	function handleInputChange(e: React.ChangeEvent<HTMLInputElement>) {
+		let inputCurrentValue = e.target.value;
+
+		// Remove any non-numeric characters except the decimal point
+		inputCurrentValue = inputCurrentValue.replace(/[^0-9.]/g, "");
+
+		// Ensure only one decimal point
+		const parts = inputCurrentValue.split(".");
+		if (parts.length > 2) {
+			inputCurrentValue = parts[0] + "." + parts.slice(1).join("");
+		}
+
+		// Handle empty input
+		if (inputCurrentValue === "") {
+			setCurrentInputValue("0");
+			return;
+		}
+
+		// Allow input ending with a decimal point
+		if (inputCurrentValue.endsWith(".")) {
+			setCurrentInputValue(inputCurrentValue);
+			return;
+		}
+
+		// Validate the number
+		const numberValue = parseFloat(inputCurrentValue);
+
+		if (!isNaN(numberValue) && numberValue >= 0) {
+			setCurrentInputValue(inputCurrentValue);
+		}
+	}
 	return (
 		<div className="flex justify-between bg-gradient-to-b from-[#0A1133] to-[#142266] rounded-[6px] px-[12px] py-[16px] w-full border-[1px] border-agyellow">
 			<div className="flex flex-col justify-start items-start gap-[8px] w-full">
 				<input
-					className="text-[32px] leading-[32px] text-agwhite font-extrabold font-sans bg-transparent w-[8ch]"
+					className="text-agwhite font-extrabold font-sans bg-transparent w-full h-full"
 					type="text"
 					value={inputValue}
-					onChange={(e) => {
-						let inputCurrentValue = e.target.value;
-
-						// Remove any non-numeric characters except the decimal point
-						inputCurrentValue = inputCurrentValue.replace(
-							/[^0-9.]/g,
-							""
-						);
-
-						// Ensure only one decimal point
-						const parts = inputCurrentValue.split(".");
-						if (parts.length > 2) {
-							inputCurrentValue =
-								parts[0] + "." + parts.slice(1).join("");
-						}
-
-						// Handle empty input
-						if (inputCurrentValue === "") {
-							setCurrentInputValue("0");
-							return;
-						}
-
-						// Allow input ending with a decimal point
-						if (inputCurrentValue.endsWith(".")) {
-							setCurrentInputValue(inputCurrentValue);
-							return;
-						}
-
-						// Validate the number
-						const numberValue = parseFloat(inputCurrentValue);
-
-						if (!isNaN(numberValue) && numberValue >= 0) {
-							setCurrentInputValue(inputCurrentValue);
-						}
+					style={{
+						fontSize:
+							fontsizeClamping(inputValue, 7, 16, 32) + "px",
+						lineHeight: 32 + "px",
 					}}
+					onChange={handleInputChange}
+					autoFocus
 				/>
-				<P extrabold className="opacity-75">
+				<P
+					style={{
+						fontSize: fontsizeClamping(inputValue, 7, 8, 16) + "px",
+						lineHeight: 16 + "px",
+					}}
+					extrabold
+					className="opacity-75 h-full flex justify-start items-center w-fit"
+				>
 					{conversion}
 				</P>
 			</div>
@@ -109,6 +119,19 @@ export function InputCard({
 	);
 }
 
+function fontsizeClamping(
+	value: string,
+	maxLengthForClamping: number,
+	minFontSize: number,
+	maxFontSize: number
+) {
+	return value.length >= maxLengthForClamping
+		? maxFontSize - (value.length - maxLengthForClamping) >= minFontSize
+			? maxFontSize - (value.length - maxLengthForClamping)
+			: minFontSize
+		: maxFontSize;
+}
+
 export function Card({
 	value,
 	conversion,
@@ -129,14 +152,29 @@ export function Card({
 	onlyValue?: boolean;
 }) {
 	return (
-		<div className="flex justify-between gap-[16px] bg-gradient-to-b from-[#0A1133] to-[#142266] rounded-[6px] px-[12px] py-[16px] w-fit min-w-full border-[1px] border-agyellow">
-			<div className="flex flex-col justify-start items-start gap-[8px] w-fit">
-				<div className="text-[32px] leading-[32px] text-agwhite font-extrabold font-sans">
+		<div className="flex justify-between gap-[16px] bg-gradient-to-b from-[#0A1133] to-[#142266] rounded-[6px] px-[12px] py-[16px] w-full min-w-full border-[1px] border-agyellow">
+			<div className="flex flex-col justify-start items-start gap-[8px] w-full">
+				<div
+					style={{
+						fontSize: fontsizeClamping(value, 7, 16, 32) + "px",
+						lineHeight: 32 + "px",
+					}}
+					className={` text-agwhite font-extrabold font-sans`}
+				>
 					{value}
 				</div>
 				{onlyValue ? null : (
-					<div className="flex opacity-75 gap-[8px]">
-						<P extrabold>{conversion}</P>
+					<div className="flex justify-center items-center opacity-75 gap-[8px]">
+						<P
+							style={{
+								fontSize:
+									fontsizeClamping(value, 7, 8, 16) + "px",
+								lineHeight: 16 + "px",
+							}}
+							extrabold
+						>
+							{conversion}
+						</P>
 						<P extrabold>x</P>
 						<P extrabold>{multiplyer}</P>
 						<Badge>Multiplied!</Badge>
@@ -236,19 +274,19 @@ export default function MiningCalculator({
 	const [selectedOption, setSelectedOption] = useState<number>(0);
 	const [USDValue, setUSDValue] = useState(value * inputOptions[0].USDvalue);
 
-  useEffect(() => {
-    console.log(selectedOption);
-    const value = Number(currentValue.replace(/,/g, ""));
-    if (!isNaN(value) && value >= 0) {
-      const usdValue = value * inputOptions[selectedOption].USDvalue;
-      setUSDValue(Number(usdValue));
-      setValue(value);
-    }
-  }, [currentValue, conversionRateToUSD, selectedOption, inputOptions]);
+	useEffect(() => {
+		console.log(selectedOption);
+		const value = Number(currentValue.replace(/,/g, ""));
+		if (!isNaN(value) && value >= 0) {
+			const usdValue = value * inputOptions[selectedOption].USDvalue;
+			setUSDValue(Number(usdValue));
+			setValue(value);
+		}
+	}, [currentValue, conversionRateToUSD, selectedOption, inputOptions]);
 
-  useEffect(() => {
-    setCurrentValue(pointsConverterToUSCommaseparated(value));
-  }, [value]);
+	useEffect(() => {
+		setCurrentValue(pointsConverterToUSCommaseparated(value));
+	}, [value]);
 
 	return (
 		<div className="relative flex flex-col gap-[8px] h-fit max-w-[400px]">
