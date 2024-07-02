0a5283dae4e9e3c85f4676e57941b7ee92b1c1d2
Pratham <chudasamapratham142@gmail.com>
2024-07-02 15:12:12 +0530

    fixed the with

--- src/components/Mining/MiningCalculator.tsx
+++ src/components/Mining/MiningCalculator.tsx
@@ -55,115 +55,71 @@ export function InputCard({
     // Validate the number
     const numberValue = parseFloat(inputCurrentValue);
 
-    if (!isNaN(numberValue) && numberValue >= 0) {
-      setCurrentInputValue(inputCurrentValue);
-    }
-  }
-  return (
-    <div className="flex justify-between gap-[8px] bg-gradient-to-b from-[#0A1133] to-[#142266] rounded-[6px] px-[12px] py-[16px] w-full border-[1px] border-agyellow">
-      <div className="flex flex-col justify-start items-start gap-[8px] w-full">
-        <form
-          onBlur={(e) => {
-            setOutOfFocus(true);
-          }}
-          onFocus={(e) => {
-            setOutOfFocus(false);
-          }}
-          onClick={() => {
-            setOutOfFocus(false);
-            inputRef.current?.focus();
-          }}
-          style={{
-            fontSize: fontsizeClamping(inputValue, 7, 16, 32) + "px",
-            lineHeight: 32 + "px",
-          }}
-          className="relative text-agwhite font-extrabold font-sans bg-transparent w-full h-fit flex justify-start items-center min-w-[8ch]"
-        >
-          <input
-            ref={inputRef}
-            className="text-agwhite font-extrabold font-sans bg-transparent w-full h-full"
-            type="number"
-            defaultValue={inputValue}
-            max={500}
-            min={10}
-            onBlur={(e) => {
-              setOutOfFocus(true);
-            }}
-            onFocus={(e) => {
-              setOutOfFocus(false);
-            }}
-            style={{
-              opacity: outOfFocus ? 0 : 1,
-              width: outOfFocus ? "0" : "100%",
-              height: outOfFocus ? "0" : "fit-content",
-              zIndex: outOfFocus ? 1 : 0,
-            }}
-            onChange={handleInputChange}
-            autoFocus
-          />
-
-          <div
-            style={{
-              opacity: outOfFocus ? 1 : 0,
-              width: outOfFocus ? "100%" : "0",
-              height: outOfFocus ? "100%" : "0",
-              zIndex: outOfFocus ? 0 : 1,
-            }}
-            className="flex justify-start items-center"
-          >
-            {inputRef &&
-              pointsConverterToUSCommaseparated(
-                Number(inputRef.current?.value)
-              )}
-          </div>
-        </form>
-
-        <P
-          style={{
-            // fontSize: fontsizeClamping(inputValue, 7, 8, 16) + "px",
-            fontSize: 16 + "px",
-            lineHeight: 16 + "px",
-          }}
-          extrabold
-          className="opacity-75 h-full flex justify-start items-center w-fit"
-        >
-          {conversion}
-        </P>
-      </div>
-      <div className="flex flex-col gap-[8px]">
-        <div
-          className={twMerge(
-            "flex justify-center items-center gap-[8px] h-full w-fit"
-          )}
-        >
-          <button className="ml-[16px] flex justify-center items-center bg-gradient-to-b from-[#B4EBF8] rounded-full to-[#789DFA] p-[1px] box-padding w-fit h-fit">
-            <div className="bg-[#0A1133] rounded-full w-fit h-fit">
-              <div className="rounded-full text-[16px] leading-[16px] px-[8px] py-[4px] from-[#B4EBF8] to-[#789DFA] font-general-sans font-semibold bg-gradient-to-b text-transparent bg-clip-text">
-                MAX
-              </div>
-            </div>
-          </button>
-          <Dropdown
-            options={dropdownOptions ?? []}
-            selected={dropDownSelected}
-            setSelected={setDropDownSelected}
-          />
-        </div>
-        <div
-          className={`flex gap-[4px] justify-end items-center text-[16px] leading-[16px] text-agwhite opacity-75 font-general-sans font-semibold`}
-        >
-          <Image
-            src={IMAGEKIT_ICONS.WALLET_WHITE}
-            alt="hammer icon"
-            width={24}
-            height={24}
-            className={twMerge("object-cover")}
-          />
-          1,245.0000 PLS
-        </div>
-      </div>
-    </div>
-  );
+		if (!isNaN(numberValue) && numberValue >= 0) {
+			setCurrentInputValue(inputCurrentValue);
+		}
+	}
+	return (
+		<div className="flex justify-between gap-[8px] bg-gradient-to-b from-[#0A1133] to-[#142266] rounded-[6px] px-[12px] py-[16px] w-fit min-w-full border-[1px] border-agyellow">
+			<div className="flex flex-col justify-start items-start gap-[8px] w-full">
+				<input
+					className="text-agwhite font-extrabold font-sans bg-transparent w-full h-full"
+					type="text"
+					value={inputValue}
+					style={{
+						fontSize:
+							fontsizeClamping(inputValue, 7, 16, 32) + "px",
+						lineHeight: 32 + "px",
+					}}
+					onChange={handleInputChange}
+					autoFocus
+				/>
+				<P
+					style={{
+						// fontSize: fontsizeClamping(inputValue, 7, 8, 16) + "px",
+						fontSize: 16 + "px",
+						lineHeight: 16 + "px",
+					}}
+					extrabold
+					className="opacity-75 h-full flex justify-start items-center w-fit"
+				>
+					{conversion}
+				</P>
+			</div>
+			<div className="flex flex-col gap-[8px]">
+				<div
+					className={twMerge(
+						"flex justify-center items-center gap-[8px] h-full w-fit"
+					)}
+				>
+					<button className="flex justify-center items-center bg-gradient-to-b from-[#B4EBF8] rounded-full to-[#789DFA] p-[1px] box-padding w-fit h-fit">
+						<div className="bg-[#0A1133] rounded-full w-fit h-fit">
+							<div className="rounded-full text-[16px] leading-[16px] px-[8px] py-[4px] from-[#B4EBF8] to-[#789DFA] font-general-sans font-semibold bg-gradient-to-b text-transparent bg-clip-text">
+								MAX
+							</div>
+						</div>
+					</button>
+					<Dropdown
+						options={dropdownOptions ?? []}
+						selected={dropDownSelected}
+						setSelected={setDropDownSelected}
+					/>
+				</div>
+				<div
+					className={`flex gap-[4px] justify-end items-center text-[16px] leading-[16px] text-agwhite opacity-75 font-general-sans font-semibold`}
+				>
+					<Image
+						src={IMAGEKIT_ICONS.WALLET_WHITE}
+						alt="hammer icon"
+						width={24}
+						height={24}
+						className={twMerge("object-cover")}
+					/>
+					1,245.0000 PLS
+				</div>
+			</div>
+		</div>
+	);
 }
 
 function fontsizeClamping(
@@ -331,59 +287,63 @@ export default function MiningCalculator({
     setCurrentValue(pointsConverterToUSCommaseparated(value));
   }, [value]);
 
-  return (
-    <div className="relative flex flex-col gap-[8px] h-fit max-w-[400px]">
-      <InputCard
-        inputValue={currentValue}
-        setCurrentInputValue={setCurrentValue}
-        conversion={`$${pointsConverterToUSCommaseparated(USDValue)}`}
-        dropdownOptions={inputOptions}
-        dropDownSelected={selectedOption}
-        setDropDownSelected={setSelectedOption}
-      />
-      <Multiplyer era={era} phase={phase} multiplyer={multiplyer} />
-      <div
-        style={{
-          gap: "11px",
-        }}
-        className="flex justify-center items-center"
-      >
-        <div
-          style={{
-            width: "100%",
-            height: "1px",
-            backgroundColor: "#FF5001",
-            borderRadius: "100px",
-          }}
-        ></div>
-        <div className="text-agwhite uppercase tracking-wider text-nowrap font-bold">
-          So you get both:
-        </div>
-        <div
-          style={{
-            width: "100%",
-            height: "1px",
-            backgroundColor: "#FF5001",
-            borderRadius: "100px",
-          }}
-        ></div>
-      </div>
-      <Card
-        value={pointsConverterToUSCommaseparated(Number(USDValue * multiplyer))}
-        conversion={`$${pointsConverterToUSCommaseparated(USDValue)}`}
-        multiplyer={pointsConverterToUSCommaseparated(multiplyer)}
-        pillIconAlt="points"
-        pillIconSrc={IMAGEKIT_ICONS.PILL_POINTS}
-        pillText="Points"
-      />
-      <Card
-        value={pointsConverterToUSCommaseparated(Number(USDValue * multiplyer))}
-        conversion={`$${pointsConverterToUSCommaseparated(USDValue)}`}
-        multiplyer={pointsConverterToUSCommaseparated(multiplyer)}
-        pillIconAlt="dark x"
-        pillIconSrc={IMAGEKIT_ICONS.PILL_DARK_X}
-        pillText="DARKX"
-      />
-    </div>
-  );
+	return (
+		<div className="relative flex flex-col gap-[8px] h-fit min-w-[400px] max-w-full">
+			<InputCard
+				inputValue={currentValue}
+				setCurrentInputValue={setCurrentValue}
+				conversion={`$${pointsConverterToUSCommaseparated(USDValue)}`}
+				dropdownOptions={inputOptions}
+				dropDownSelected={selectedOption}
+				setDropDownSelected={setSelectedOption}
+			/>
+			<Multiplyer era={era} phase={phase} multiplyer={multiplyer} />
+			<div
+				style={{
+					gap: "11px",
+				}}
+				className="flex justify-center items-center"
+			>
+				<div
+					style={{
+						width: "100%",
+						height: "1px",
+						backgroundColor: "#FF5001",
+						borderRadius: "100px",
+					}}
+				></div>
+				<div className="text-agwhite uppercase tracking-wider text-nowrap font-bold">
+					So you get both:
+				</div>
+				<div
+					style={{
+						width: "100%",
+						height: "1px",
+						backgroundColor: "#FF5001",
+						borderRadius: "100px",
+					}}
+				></div>
+			</div>
+			<Card
+				value={pointsConverterToUSCommaseparated(
+					Number((USDValue * multiplyer).toFixed(20))
+				)}
+				conversion={`$${pointsConverterToUSCommaseparated(USDValue)}`}
+				multiplyer={pointsConverterToUSCommaseparated(multiplyer)}
+				pillIconAlt="points"
+				pillIconSrc={IMAGEKIT_ICONS.PILL_POINTS}
+				pillText="Points"
+			/>
+			<Card
+				value={pointsConverterToUSCommaseparated(
+					Number((USDValue * multiplyer).toFixed(20))
+				)}
+				conversion={`$${pointsConverterToUSCommaseparated(USDValue)}`}
+				multiplyer={pointsConverterToUSCommaseparated(multiplyer)}
+				pillIconAlt="dark x"
+				pillIconSrc={IMAGEKIT_ICONS.PILL_DARK_X}
+				pillText="DARKX"
+			/>
+		</div>
+	);
 }
