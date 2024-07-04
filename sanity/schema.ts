19e87ac6d5256fa96221769d8ae149e2a365fddc
Angel Lakra <angel.lakra@chainlabs.in>
2024-07-04 17:42:53 +0530

    Added timestamps in cms

--- sanity/schema.ts
+++ sanity/schema.ts
@@ -3,7 +3,14 @@ import { collectiveType } from "./schemaTypes/collective";
 import { eventType } from "./schemaTypes/event";
 import { communityType } from "./schemaTypes/community";
 import { teamMembersType } from "./schemaTypes/teamMember";
+import { timestampType } from "./schemaTypes/timestamps";
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [collectiveType, eventType, communityType, teamMembersType],
+  types: [
+    collectiveType,
+    eventType,
+    communityType,
+    teamMembersType,
+    timestampType,
+  ],
 };
