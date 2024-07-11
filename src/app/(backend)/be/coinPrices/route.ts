6c8556f1aab5c6ab7d229ce295d86629dd6939f7
Angel Lakra <angel.lakra@chainlabs.in>
2024-07-11 15:59:21 +0530

    added compatible tokens

--- src/app/(backend)/be/coinPrices/route.ts
+++ src/app/(backend)/be/coinPrices/route.ts
@@ -37,18 +37,18 @@ export async function GET(request: NextRequest) {
   } catch (err) {
     console.log({ err });
     switch (token) {
-      case "0x72d9F40C7De5F6F2aB699EAA0ca7c66f937C37b9":
-      case "0x3A927E1E8D15A8bb676535F1514106683293d153": {
+      case "0xd8653339bff4A18108e9BFC2d5d15C90a4F04eA5":
+      case "0xaBA9FaCcf99C36D61A7C855ED77a58FC49151d5A": {
         price = 1;
         break;
       }
-      case "0x8B4b597BC078b19be4454062064F446E850afa5f":
-      case "0xaf177f24CF0388c698571AcB6E3f1DE50026155A": {
+      case "0xaBA9FaCcf99C36D61A7C855ED77a58FC49151d5A":
+      case "0x95C835EE2B7560E7e5116d533795548Ea6367313": {
         price = 0.0195;
         break;
       }
-      case "0x7EDA2eB75892922D809C0A73518957594A5043cE":
-      case "0x650A08cc7BafD06C4c16D8dbb0eE755aD7480Bf9": {
+      case "0x11334A36427C607Aa07bd607DbBB27fcDa6db6AB":
+      case "0x3256e547407F63C862EC0732272028534B6676d2": {
         price = 0.000000000029;
         break;
       }
