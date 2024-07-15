32f786008408873b611cb8205a2dd7f3ded215e5
Angel Lakra <angel.lakra@chainlabs.in>
2024-07-15 22:15:12 +0530

    updated contracts and changed structure of the contracts hooks

--- src/abi/Dark/index.ts
+++ src/abi/Dark/index.ts
@@ -11,11 +11,11 @@ import abi from "./abi.json";
 
 const contracts: Record<number, { address: `0x${string}`; abi: any }> = {
   [sepolia.id]: {
-    address: "0x53521B27E4cfDbF2a7de2ff6b4D24c168792cB14",
+    address: "0xc93fb3e7C6525a6D3A92016594fc3aEA7B12F0Db",
     abi,
   },
   [pulsechain.id]: {
-    address: "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE",
+    address: "0xc93fb3e7C6525a6D3A92016594fc3aEA7B12F0Db",
     abi,
   },
 };
--- src/abi/DarkX/index.ts
+++ src/abi/DarkX/index.ts
@@ -1,5 +1,5 @@
 import { TEST_NETWORK } from "@/constants";
-import { baseSepolia, pulsechain, sepolia } from "viem/chains";
+import { base, baseSepolia, pulsechain, sepolia } from "viem/chains";
 import { useAccount } from "wagmi";
 
 interface IContract {
@@ -9,28 +9,38 @@ interface IContract {
 
 import abi from "./abi.json";
 
+const contracts: Record<number, { address: `0x${string}`; abi: any }> = {
+  [sepolia.id]: {
+    address: "0x74E33af8ffa59B68402e5c4767c37416827D5D2E",
+    abi,
+  },
+  [baseSepolia.id]: {
+    address: "0x63DDC2ACab1Fb8871F19A2B89b50c1F99C21D718",
+    abi,
+  },
+  [pulsechain.id]: {
+    address: "0x74E33af8ffa59B68402e5c4767c37416827D5D2E",
+    abi,
+  },
+  [base.id]: {
+    address: "0x74E33af8ffa59B68402e5c4767c37416827D5D2E",
+    abi,
+  },
+};
+
 const useDarkXContract = (): IContract => {
   const account = useAccount();
   if (TEST_NETWORK) {
     if (account.chain?.id === baseSepolia.id) {
-      // Change the address here
-      return {
-        address: "0xC719b2a61Cf9D0185d23b316f9EF7e28845c2274",
-        abi,
-      };
+      return contracts[baseSepolia.id];
     } else if (account.chain?.id === sepolia.id) {
-      return {
-        address: "0xD7ae623f3e0ceF54e7447513A7f47571dec09e0B",
-        abi,
-      };
+      return contracts[sepolia.id];
     }
   } else {
     if (account.chain?.id === pulsechain.id) {
-      // Change the address here
-      return {
-        address: "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE",
-        abi,
-      };
+      return contracts[pulsechain.id];
+    } else if (account.chain?.id === base.id) {
+      return contracts[base.id];
     }
   }
 
