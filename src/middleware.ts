505aed6e3883b70335e64b0e07c82ff1dcdde062
Pratham <chudasamapratham142@gmail.com>
2024-07-16 15:42:05 +0530

    blocked url excluded in check

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,20 +2,23 @@ import { NextRequest, NextResponse } from "next/server";
 import { headers } from "next/headers";
 
 export async function middleware(request: NextRequest) {
-	const forwardedFor = headers().get("x-forwarded-for");
-	const realIp = headers().get("x-real-ip");
+  const forwardedFor = headers().get("x-forwarded-for");
+  const realIp = headers().get("x-real-ip");
 
-	const apiResponse = await (
-		await fetch(
-			`https://get.geojs.io/v1/ip/country/${realIp ?? "0.0.0.0"}.json`
-		)
-	).json();
+  const apiResponse = await (
+    await fetch(
+      `https://get.geojs.io/v1/ip/country/${realIp ?? "0.0.0.0"}.json`,
+    )
+  ).json();
 
-	const restrictedCountryCodes = ["GUM", "USA", "VIR", "PRI"];
+  const restrictedCountryCodes = ["GUM", "USA", "VIR", "PRI"];
 
-	if (restrictedCountryCodes.includes(apiResponse.country_3) && request.url !== "/blocked") {
-		return NextResponse.redirect(new URL('/blocked', request.url));
-	}
+  if (
+    restrictedCountryCodes.includes(apiResponse.country_3) &&
+    !request.url.includes("/blocked")
+  ) {
+    return NextResponse.redirect(new URL("/blocked", request.url));
+  }
 }
 
 // GUM, USA, VIR, PRI
