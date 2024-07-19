d875d2321a5c8c62ba6fadeeb757575e3ea735cf
Angel Lakra <angel.lakra@chainlabs.in>
2024-07-19 19:37:29 +0530

    added logs

--- src/components/Mining/Hero/NFTPopUp.tsx
+++ src/components/Mining/Hero/NFTPopUp.tsx
@@ -46,6 +46,8 @@ export default function NFTPopUp({
 
   useEffect(() => {
     if (account.address) {
+      console.log({ nftURLera2 });
+
       setPoints({
         wishwell: wishwellPoints,
         mining: miningPoints,
