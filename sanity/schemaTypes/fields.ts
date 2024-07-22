fcb2440ff5800975ad665c23b2bb0705797c2df5
Pratham <chudasamapratham142@gmail.com>
2024-07-22 16:15:21 +0530

    disabled state for book url added

--- sanity/schemaTypes/fields.ts
+++ sanity/schemaTypes/fields.ts
@@ -89,7 +89,6 @@ export const bookFields = [
     title: "Book URL",
     type: "url",
     group: "book",
-    validation: (rule) => rule.required(),
   }),
   defineField({
     name: "book_image",
